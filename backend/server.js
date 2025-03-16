1246c812cb874b0268af1b462ffc2f864da8dbec
Afthar17 <aftharafthar5@gmail.com>
2025-03-16 14:25:42 +0530

    for production commit

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,41 @@
-import express from 'express';
-import dotenv from "dotenv"
-import authRoutes from './routes/auth.route.js'
-import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser'
-import productRoutes from './routes/productRoutes.js';
-import cartRoutes from './routes/cartRoutes.js'
-import couponRoutes from './routes/couponRoutes.js'
-import paymentRoutes from './routes/paymentRoutes.js'
-import analyticsRoutes from './routes/analyticsRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import couponRoutes from "./routes/couponRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
+connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-connectDB()
-const app = express()   
-const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
-app.use(express.json({limit: '10mb'}))
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use('/api/auth',authRoutes)
-app.use('/api/products',productRoutes)
-app.use('/api/cart',cartRoutes)
-app.use('/api/coupons',couponRoutes)
-app.use('/api/payments',paymentRoutes)
-app.use('/api/analytics',analyticsRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-    
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
